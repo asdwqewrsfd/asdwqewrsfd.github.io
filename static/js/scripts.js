d5b3a424613df59c2cec754b5d883bf8b302aa13
asdwqewrsfd <ruiqingzang@gmail.com>
2025-02-04 20:29:40 -0800

    1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards','research']
+const section_names = ['home', 'pub', 'publications', 'awards','research']
 
 
 window.addEventListener('DOMContentLoaded', event => {
